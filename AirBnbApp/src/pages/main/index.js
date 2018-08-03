86065f2b2902640f041c3de50c23ed506ff8737b
Douglas Alves <doug-pr@hotmail.com>
2018-08-03 17:39:28 -0300

    Adicionado telas do React-Native

---
+++ AirBnbApp/src/pages/main/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import View from 'react-native'
+
+const main = () => (
+    <View />
+)
+
+export default Main
--- airbnb-server/start/routes.js
+++ airbnb-server/start/routes.js
@@ -4,7 +4,11 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+Route.post('properties/:id/images', 'ImageController.store')
+    .middleware('auth')
 
-Route.resource('porperties', 'PropertyController')
+Route.get('images/:path', 'ImageController.show')
+
+Route.resource('properties', 'PropertyController')
     .apiOnly()
     .middleware('auth')
